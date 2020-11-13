2c91569afff4e41c80f6774511a37ca05cf990e1
MiwelR <rrriosss@gmail.com>
2020-11-13 19:33:10 +0100

    Entrada - Control de Eventos

--- main.py
+++ main.py
@@ -1,11 +1,116 @@
 import pygame, sys
 from pygame.locals import *
 
-
 class Termometro():
     def __init__(self):
         self.custome = pygame.image.load("images/termo1.png")
 
+class NumberInput():
+    __value = 0
+    __strValue = "0"
+    __position = [0, 0]
+    __size = [0, 0]
+    
+    def __init__(self, value=0):
+        self.__font = pygame.font.SysFont("Arial", 24)
+        self.value(value)
+        
+        '''
+        try:
+            self.__strValue = int(value)
+            self.__strValue = str(value)
+        except:
+            pass
+        '''   
+    
+    def on_event(self, event):
+        if event.type == KEYDOWN:
+            if event.unicode.isdigit() and len(self.__strValue) < 12:# Tambien podemos usar: if event.unicode in '0123456789'
+                self.__strValue += event.unicode
+            elif event.key == K_BACKSPACE:
+                self.__strValue = self.__strValue[:-1]
+            
+    
+    def render(self):
+        textBlock = self.__font.render(self.__strValue, True, (74, 74, 74))
+        rect = textBlock.get_rect()
+        rect.left = self.__position[0]
+        rect.top = self.__position[1]
+        rect.size = self.__size
+        
+        # Otra forma de hacer el return:
+        '''
+        return {
+                "fondo": rect,
+                "texto": textBlock
+            }
+        '''
+        return (rect, textBlock)
+    
+    def value(self, val=None):
+        if val==None:
+            return self.__value
+        else:
+            val = str(val)
+            try:
+                self.__value = int(val)
+                self.__strValue = val
+            except:
+                pass
+            
+    def width(self, val=None):
+        if val == None:
+            return self.__size[0]
+        else:
+            try:
+                self.__size[0] = int(val)
+            except:
+                pass
+     
+    def height(self, val=None):
+        if val == None:
+            return self.__size[1]
+        else:
+            try:
+                self.__size[1] = int(val)
+            except:
+                pass
+            
+    def size(self, val=None):
+        if val == None:
+            return self.__size
+        else:
+            try:
+                self.__size = [int(val[0]), int(val[1])]
+            except:
+                pass
+
+    def posX(self, val=None):
+        if val == None:
+            return self.__position[0]
+        else:
+            try:
+                self.__position[0] = int(val)
+            except:
+                pass
+     
+    def posY(self, val=None):
+        if val == None:
+            return self.__position[1]
+        else:
+            try:
+                self.__position[1] = int(val)
+            except:
+                pass
+            
+    def pos(self, val=None):
+        if val == None:
+            return self.__position
+        else:
+            try:
+                self.__position = [int(val[0]), int(val[1])]
+            except:
+                pass
 
 class mainApp():
     termometro = None
@@ -18,6 +123,10 @@ class mainApp():
         self.__screen.fill((244, 236, 203))
         
         self.termometro = Termometro()
+        self.entrada = NumberInput()
+        self.entrada.pos((106, 58))
+        self.entrada.size((133, 28))
+        
     
     def __on_close(self):
         pygame.quit()
@@ -29,7 +138,16 @@ class mainApp():
                 if event.type == QUIT:
                     self.__on_close()
                     
+                self.entrada.on_event(event)
+                
+            # Pintamos el termómetro en su posición
             self.__screen.blit(self.termometro.custome, (50, 34))
+            
+            # Pintamos el cuadro de texto
+            text = self.entrada.render()# Obtenemos rectángulo blanco y foto del texto y lo asignamos a text
+            pygame.draw.rect(self.__screen, (255, 255, 255), text[0]) # Creamos el rectángulo blanco con sus datos (posición y tamaño) text[0]
+            self.__screen.blit(text[1], self.entrada.pos()) # Pintamos la foto del texto (text[1])
+            
             pygame.display.flip()
     
         
